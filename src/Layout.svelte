19e919699b248441a20e6c108ab63567d2afac74
Gaff <gaff@users.noreply.github.com>
2026-01-04 10:19:52 +0000

    Shitty attemp at theming.

---
+++ src/Layout.svelte
@@ -0,0 +1,17 @@
+<main class="container">
+	<header>
+		<hgroup>
+			<h1>PGP Help</h1>
+			<p>Simple client-side encryption.</p>
+		</hgroup>
+		<nav>
+			<a href="https://pgp.help" target="_blank" rel="noopener">Help</a>
+		</nav>
+	</header>
+
+	<slot />
+
+	<footer>
+		<p>View source on <a href="https://github.com/yourusername/pgp-svelte-wsl" target="_blank" rel="noopener">GitHub</a></p>
+	</footer>
+</main>
